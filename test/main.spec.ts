38c24faaced99f981ba40c7a6b57bb915b2bd720
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2022-10-01 19:51:22 +0200

    up

--- test/main.spec.ts
+++ test/main.spec.ts
@@ -1,15 +1,18 @@
 import BigNumber from 'bignumber.js';
-import { set_mockup, get_endpoint, is_mockup } from '../src';
+import { set_mockup, set_endpoint, get_endpoint, is_mockup } from '../src';
 
 const assert = require('assert');
 
 describe('Mockup', () => {
-  it('set_mockup', () => {
-    set_mockup()
+  it('set_endpoint', () => {
+    set_endpoint('mockup');
   })
   it('get_endpoint', () => {
     const endpoint = get_endpoint();
     assert(endpoint == 'mockup')
+  })
+  it('set_mockup', () => {
+    set_mockup()
   })
   it('set_mockup', () => {
     assert(is_mockup())
